13217ae5e6dcd472ae4e2ece2155e9ce3123b2f6
shishirbhargav09 <sbhargav2000@gmail.com>
2022-11-26 22:54:21 +0530

    5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-
+import { store } from "./Store/store";
 import Navbar from "./Components/Navbar";
 import NoPage from "./Pages/NoPage";
 import Cart from "./Pages/Cart";
